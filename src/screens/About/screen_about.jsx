c5896dddbc9d3f66febdcb239ac29453a80ae5de
Abnerpino <abnerpino15@gmail.com>
2025-04-14 12:20:59 -0600

    Se implementó un fondo personalizado y se agregó el icono de la aplicación

--- src/screens/About/screen_about.jsx
+++ src/screens/About/screen_about.jsx
@@ -1,38 +1,48 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const About = ({ navigation }) => {
 
     return (
-        <View style={{ flex: 1, backgroundColor: '#000' }}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingHorizontal: 15, paddingVertical: 12.5, alignSelf: 'flex-start' }}>
-                    <Icon name="arrow-circle-left" size={26} color="white" />
-                </TouchableOpacity>
-                <Text style={styles.sectionTitle}>ACERCA DE</Text>
-            </View>
-            <View style={styles.body}>
-                <Image
-                    source={require('../../assets/imagotipo.png')}
-                    style={{ height: '30%', width: '30%', resizeMode: 'contain' }}
-                />
-                <Text style={styles.version}>Versión: 1.0</Text>
-                <Text style={styles.description}>IPTV Player es una aplicación móvil para ver canales en vivo, películas y series, todo a través de una conexión a Internet.</Text>
-                <View style={{ flexDirection: 'row', paddingVertical: 20 }}>
-                    <View style={{ alignItems: 'flex-start', paddingRight: 25 }}>
-                        <Text style={[styles.info, { fontWeight: 'bold' }]}>Desarrollador:</Text>
-                        <Text style={[styles.info, { fontWeight: 'bold' }]}>Correo:</Text>
-                        <Text style={[styles.info, { fontWeight: 'bold' }]}>Lanzamiento:</Text>
-                    </View>
-                    <View style={{ alignItems: 'flex-start', paddingLeft: 25 }}>
-                        <Text style={styles.info}>Ing. Abner Pino Federico</Text>
-                        <Text style={styles.info}>abnerpino15@gmail.com</Text>
-                        <Text style={styles.info}>2025</Text>
-                    </View>
+        <ImageBackground
+            source={require('../../assets/fondo3.jpg')}
+            style={{
+                flex: 1,
+                width: '100%',
+                height: '100%',
+            }}
+            resizeMode='cover'
+        >
+            <View style={{ flex: 1, backgroundColor: 'rgba(16,16,16,0)', }}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={{ paddingHorizontal: 15, paddingVertical: 12.5, alignSelf: 'flex-start' }}>
+                        <Icon name="arrow-circle-left" size={26} color="white" />
+                    </TouchableOpacity>
+                    <Text style={styles.sectionTitle}>ACERCA DE</Text>
                 </View>
+                <View style={styles.body}>
+                    <Image
+                        source={require('../../assets/icono.jpg')}
+                        style={{ height: '30%', width: '30%', resizeMode: 'contain' }}
+                    />
+                    <Text style={styles.version}>Versión: 1.0</Text>
+                    <Text style={styles.description}>IPTV Player es una aplicación móvil para ver canales en vivo, películas y series, todo a través de una conexión a Internet.</Text>
+                    <View style={{ flexDirection: 'row', paddingVertical: 20 }}>
+                        <View style={{ alignItems: 'flex-start', paddingRight: 25 }}>
+                            <Text style={[styles.info, { fontWeight: 'bold' }]}>Desarrollador:</Text>
+                            <Text style={[styles.info, { fontWeight: 'bold' }]}>Correo:</Text>
+                            <Text style={[styles.info, { fontWeight: 'bold' }]}>Lanzamiento:</Text>
+                        </View>
+                        <View style={{ alignItems: 'flex-start', paddingLeft: 25 }}>
+                            <Text style={styles.info}>Ing. Abner Pino Federico</Text>
+                            <Text style={styles.info}>abnerpino15@gmail.com</Text>
+                            <Text style={styles.info}>2025</Text>
+                        </View>
+                    </View>
 
+                </View>
             </View>
-        </View>
+        </ImageBackground>
     );
 }
 
