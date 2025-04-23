1d66350592d976998a43804e7f741e8fe99be2b6
Abnerpino <abnerpino15@gmail.com>
2025-04-22 18:24:02 -0600

    Se cambiaron y agregaron metodos para el manejo de la información en PocketBase

--- src/services/controllers/hostingController.js
+++ src/services/controllers/hostingController.js
@@ -1,30 +1,44 @@
 import pb from '../hosting/pocketbase';
 
 class HostingController {
-    async registrarDispositivo(data) {
+    async registrarCliente(data) {
         try {
-            const record = await pb.collection('clients').create(data); 
+            await pb.collection('clients').create(data);
+            return true;
         } catch (error) {
             console.error("Error al registrar cliente: ", error);
-            throw new Error("Error al registrar cliente: " + error.message);
+            return null;
         }
     }
 
-    async obtenerDispositivo(deviceId) {
+    async verificarCliente(deviceId) {
         try {
-            const records = await pb.collection('clients').getList(1, 50, {
-                filter: `device_id="${deviceId}"`,
-            });
-    
-            if (records.items.length > 0) {
-                console.log('El device_id existe:');
-                return records.items[0];
-            } else {
-                console.log('El device_id no existe.');
-                return null;
+            const allRecords = await pb.collection('clients').getFullList(); //Obtiene la lista completa de los clientes
+            const result = allRecords.find(record => record.device_id === deviceId); //Busca si ya existe algún cliente con el id del dispositivo
+
+            if (result) { //Si ya existe...
+                return result; //Retorna toda la información del cliente
+                console.log("Ya existe el device_id");
+            } else { //Si todavía no existe
+                const users = allRecords.map(record => record.user_name); //Genera un nuevo arreglo de solo nombres de usuario
+                return users; //Retorna los usuarios
             }
         } catch (error) {
-            console.error("Error al obtener el ID del dispositivo");
+            console.error("Error al obtener el ID del dispositivo: ", error);
+            return null; //Retorna null si falla la petición
+        }
+    }
+
+    async obtenerNotificaciones(id) {
+        try {
+            const allRecords = await pb.collection('notifications').getFullList({
+                filter: `client_id = "${id}"`
+            }); //Obtiene todos los registros relacionados con el cliente
+            const notifications = allRecords.map(record => record.message); //Genera un nuevo arreglo de solo las notificaciones del cliente
+            return notifications;
+        } catch (error) {
+            console.log('Error al obtener las notificaciones: ', error);
+            return null;
         }
     }
 }
