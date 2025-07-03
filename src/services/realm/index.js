a51bf482fd6206cd73233f0ccf5eb009f3cce9ac
Abnerpino <abnerpino15@gmail.com>
2025-07-03 15:09:28 -0600

    Configuraciones para el funcionamiento de Realm.

---
+++ src/services/realm/index.js
@@ -0,0 +1,14 @@
+import Realm from 'realm';
+import { CanalSchema, PeliculaSchema, SerieSchema, TemporadaSchema, EpisodioSchema } from '../schemas/schemasStreaming';
+
+let realmInstance;
+
+export const getRealm = () => {
+  if (!realmInstance) {
+    realmInstance = new Realm({
+      schema: [CanalSchema, PeliculaSchema, SerieSchema, TemporadaSchema, EpisodioSchema],
+      schemaVersion: 1,
+    });
+  }
+  return realmInstance;
+};
---
+++ src/services/realm/streaming.js
@@ -0,0 +1,68 @@
+import { getRealm } from './index';
+
+const realm = getRealm();
+
+export const saveItems = (type, data) => {
+  let idContenido = -1;
+  const model = getModelName(type);
+  realm.write(() => {
+    // Limpia primero para evitar duplicados
+    const old = realm.objects(model);
+    realm.delete(old);
+
+    try {
+      data.forEach(item => {
+        const id = type === 'series' ? item.series_id : item.stream_id;
+        if (id !== idContenido) {
+          realm.create(model, item);
+          idContenido = id;
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+export const getItems = (type) => {
+  const model = getModelName(type);
+  return realm.objects(model).sorted('num');
+};
+
+export const getItemById = (type, id) => {
+  const model = getModelName(type);
+  return realm.objectForPrimaryKey(model, id);
+};
+
+export const updateItem = (type, idKey, idValue, changes) => {
+  const model = getModelName(type);
+  realm.write(() => {
+    const item = realm.objectForPrimaryKey(model, idValue);
+    if (item) {
+      Object.entries(changes).forEach(([key, value]) => {
+        item[key] = value;
+      });
+    }
+  });
+};
+
+export const marckEpisodeAsWatched = (serieId, episodioId) => {
+  const serie = realm.objectForPrimaryKey('Serie', serieId);
+  if (!serie) {
+    console.log('No se encontró la serie');
+    return;
+  } else console.log('Serie encontrada');
+
+  realm.write(() => {
+    const episodio = serie.episodios.find(e => e.id === episodioId);
+    if (episodio) {
+      episodio.visto = true;
+    }
+  });
+};
+
+export const getModelName = (type) => {
+  if (type === 'live') return 'Canal';
+  if (type === 'vod') return 'Pelicula';
+  return 'Serie';
+};
