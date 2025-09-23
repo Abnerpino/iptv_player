abec73d72b51405ade1defc0b3ad3321a169b10b
Abnerpino <abnerpino15@gmail.com>
2025-09-23 13:52:33 -0600

    Se agregaron métodos para guardar las propiedades en los schemas auxiliares y tambien para copiar esas propiedades a los shcemas principales cuando estos se actualicen.

--- src/services/realm/streaming.js
+++ src/services/realm/streaming.js
@@ -24,6 +24,20 @@ export const saveItems = (type, data) => {
   });
 };
 
+// Método para guardar o actualizar las propiedades 'Favorito', 'Visto' y 'Temporada' (cuando es Serie) en los items de los schemas auxiliares
+export const saveOrUpdateItems = (type, item) => {
+  const model = getModelName(type);
+  realm.write(() => {
+    try {
+      // El parámetro 'modified' le indica a Realm que actualice un objeto si existe, o lo cree si es nuevo.
+      // Solo los campos que han cambiado en 'item' serán actualizados.
+      realm.create(model, item, 'modified');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
 export const getItems = (type) => {
   const model = getModelName(type);
   return realm.objects(model).sorted('num');
@@ -34,6 +48,7 @@ export const getItemById = (type, id) => {
   return realm.objectForPrimaryKey(model, id);
 };
 
+// Método para actualizar las propiedades de los schemas principales
 export const updateItem = (type, idKey, idValue, changes) => {
   const model = getModelName(type);
   realm.write(() => {
@@ -42,6 +57,41 @@ export const updateItem = (type, idKey, idValue, changes) => {
       Object.entries(changes).forEach(([key, value]) => {
         item[key] = value;
       });
+    }
+  });
+};
+
+// Método para actualizar las propiedades de los items de un schema principal usando las propiedades de esos items en su schema auxiliar
+export const updateItemPropsInSchema = (typeAux, type) => {
+  const items = getItems(typeAux);  // Obtiene los items del schema auxiliar
+
+  for (const item of items) {
+    if (type === 'series') {
+      updateItem(type, 'series_id', item.series_id, { temporadas: item.temporadas, favorito: item.favorito, visto: item.visto });  // Actualiza las propiedades del item en su schema principal
+    } else { // Canales y Peliculas
+      updateItem(type, 'stream_id', item.stream_id, { favorito: item.favorito, visto: item.visto });  // Actualiza las propiedades del item en su schema principal
+    }
+  }
+};
+
+// Método para eliminar items de los schemas auxiliares
+export const deleteItem = (type, idValue) => {
+  const model = getModelName(type);
+
+  realm.write(() => {
+    try {
+      // Busca el objeto directamente por su clave primaria.
+      const itemToDelete = realm.objectForPrimaryKey(model, idValue);
+
+      // Si se encontró el objeto, se elimina.
+      if (itemToDelete) {
+        realm.delete(itemToDelete);
+        console.log('Ítem eliminado exitosamente.');
+      } else {
+        console.log('No se encontró el ítem para eliminar.');
+      }
+    } catch (error) {
+      console.error('Error al eliminar el ítem:', error);
     }
   });
 };
@@ -70,5 +120,8 @@ export const marckEpisodeAsWatched = (serieId, numSeason, episodioId) => {
 export const getModelName = (type) => {
   if (type === 'live') return 'Canal';
   if (type === 'vod') return 'Pelicula';
-  return 'Serie';
+  if (type === 'series') return 'Serie'
+  if (type === 'auxLive') return 'CanalAux';
+  if (type === 'auxVod') return 'PeliculaAux';
+  return 'SerieAux';
 };
