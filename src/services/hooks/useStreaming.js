c3c847f523b0293307660e952b87a82bc34f6dbd
Abnerpino <abnerpino15@gmail.com>
2025-10-06 22:17:49 -0600

    refactor(streaming): transformar a hook el archivo streaming.js para usar la nueva librería de Realm

---
+++ src/services/hooks/useStreaming.js
@@ -0,0 +1,149 @@
+import { useRealm } from "@realm/react";
+
+export const useStreaming = () => {
+    const realm = useRealm();
+
+    const upsertContentItems = (type, newItems) => {
+        const model = getModelName(type);
+        const idField = type === 'series' ? 'series_id' : 'stream_id';
+
+        realm.write(() => {
+            const allItemsOfType = realm.objects(model);
+            const newItemsIds = newItems.map(item => item[idField]); //Genera un nuevo arreglo de solo los id´s de los nuevos items
+            const missingItems = allItemsOfType.filtered(`NOT (${idField} IN $0)`, newItemsIds); //Filtra los items actuales que ya no se encuentran en los nuevos items
+            realm.delete(missingItems); //Elimina los items faltantes
+
+            newItems.forEach(item => {
+                const oldItem = realm.objectForPrimaryKey(model, item[idField]);
+                let dataToSave = { ...item };
+
+                // Preserva estado de las propiedades 'Visto', 'Favorito' y 'Temporadas'
+                if (oldItem) {
+                    dataToSave.visto = oldItem.visto;
+                    dataToSave.favorito = oldItem.favorito;
+                    if (type === 'series' && oldItem.temporadas && oldItem.visto) {
+                        dataToSave.temporadas = oldItem.temporadas.toJSON();
+                    }
+                }
+                realm.create(model, dataToSave, 'modified');
+            });
+        });
+    };
+
+    const syncStreamingData = (type, categories, items) => {
+        return new Promise((resolve, reject) => {
+            try {
+                // 1. Guardar o actualizar todos los items de contenido (Canales, Películas, Series)
+                upsertContentItems(type, items);
+
+                // 2. Guardar o actualizar las categorías y vincular su contenido
+                const categoryModel = getModelName(type, true); // 'CatsLive', 'CatsVod', 'CatsSerie'
+                const contentModel = getModelName(type); // 'Canal', 'Pelicula', 'Serie'
+                const contentField = type === 'live' ? 'canales' : (type === 'vod' ? 'peliculas' : 'series');
+
+                realm.write(() => {
+                    categories.forEach(cat => {
+                        // Filtrar los items que pertenecen a esta categoría
+                        let itemsForThisCategory = [];
+
+                        switch (cat.category_id) {
+                            case '0.1':
+                                itemsForThisCategory = realm.objects(contentModel);
+                                break;
+                            case '0.2':
+                                itemsForThisCategory = realm.objects(contentModel).filtered('visto == true');
+                                break;
+                            case '0.3':
+                                itemsForThisCategory = realm.objects(contentModel).filtered('favorito == true');
+                                break;
+                            default:
+                                itemsForThisCategory = realm.objects(contentModel).filtered('$0 IN category_ids', cat.category_id);
+                        }
+
+                        // Crear o actualizar la categoría en Realm
+                        realm.create(categoryModel, {
+                            category_id: cat.category_id,
+                            category_name: cat.category_name,
+                            total: itemsForThisCategory.length,
+                            [contentField]: itemsForThisCategory, // Asigna la lista de items vinculados
+                        }, 'modified');
+                    });
+                });
+
+                resolve();
+            } catch (error) {
+                console.log(`Error al sincronizar datos en Realm (${type}):`, error);
+                reject(error);
+            }
+        });
+    };
+
+    // Método para obtener los items vistos de una colección, ordenados por la propiedad 'num
+    const getWatchedItems = (type) => {
+        const model = getModelName(type);
+        const items = realm.objects(model).sorted('num');
+        return items.filtered('visto == true');
+    };
+
+    // Método para obtener los items vistos de una colección, ordenados por la propiedad 'num
+    const getFavoriteItems = (type) => {
+        const model = getModelName(type);
+        const items = realm.objects(model).sorted('num');
+        return items.filtered('favorito == true');
+    };
+
+    // Método para actualizar las propiedades de un item o categoría
+    const updateProps = (type, flag, idKey, idValue, changes) => {
+        const model = getModelName(type, flag);
+        realm.write(() => {
+            const item = realm.objectForPrimaryKey(model, idValue);
+            if (item) {
+                Object.entries(changes).forEach(([key, value]) => {
+                    item[key] = value;
+                });
+            }
+        });
+    };
+
+    // Método para marcar un episodio como 'Visto'
+    const marckEpisodeAsWatched = (serieId, numSeason, episodioId) => {
+        const serie = realm.objectForPrimaryKey('Serie', serieId);
+        if (!serie) {
+            console.log('No se encontró la serie');
+            return;
+        } else console.log('Serie encontrada');
+
+        const temporada = serie.temporadas.find(t => t.numero === numSeason);
+        if (!temporada) {
+            console.log('No se encontró la temporada');
+            return;
+        } else console.log('Temporada encontrada');
+
+        realm.write(() => {
+            const episodio = temporada.episodios.find(e => e.id === episodioId);
+            if (episodio) {
+                episodio.visto = true;
+            }
+        });
+    };
+
+    const getModelName = (type, forCategory = false) => {
+        if (forCategory) {
+            if (type === 'live') return 'CatsLive';
+            if (type === 'vod') return 'CatsVod';
+            return 'CatsSerie';
+        }
+        if (type === 'live') return 'Canal';
+        if (type === 'vod') return 'Pelicula';
+        return 'Serie';
+    };
+
+    return {
+        syncStreamingData,
+        getWatchedItems,
+        getFavoriteItems,
+        updateProps,
+        marckEpisodeAsWatched,
+        getModelName,
+    };
+};
