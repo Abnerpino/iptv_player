80c3e5f0c7e860c53d0570d43025c20546c9c233
Abnerpino <abnerpino15@gmail.com>
2025-11-11 23:41:18 -0600

    chore: eliminar de Redux el slice de Notificacion para migrarlo a Realm

--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -3,7 +3,6 @@ import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { combineReducers } from 'redux';
 import clientReducer from './slices/clientSlice';
-import notificationsReducer from './slices/notificationsSlice';
 
 const persistConfig = {
   key: 'root',
@@ -12,7 +11,6 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   client: clientReducer,
-  notifications: notificationsReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
