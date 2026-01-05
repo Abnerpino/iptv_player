bbe262c7da5831e3478f2c511c36179f71a75034
Abnerpino <abnerpino15@gmail.com>
2026-01-05 17:54:49 -0600

    style(card_actor): implementar hoja de estilos

--- src/components/Cards/card_actor.jsx
+++ src/components/Cards/card_actor.jsx
@@ -1,46 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
 const CardActor = ({ imagen, nombre }) => {
     return (
-        <View style={{ position: 'relative', }}>
+        <View style={styles.container}>
             <FastImage
                 source={{
                     uri: imagen,
                     priority: FastImage.priority.normal
                 }}
-                style={{
-                    width: 100,
-                    height: 150,
-                    backgroundColor: '#201F29',
-                    borderRadius: 5,
-                    borderColor: '#fff',
-                    borderWidth: 0.5
-                }}
+                style={styles.imagen}
                 resizeMode={FastImage.resizeMode.cover}
             />
-            <View style={{
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                right: 0,
-                backgroundColor: 'rgba(0, 0, 0, 0.15)',
-                padding: 5,
-                borderBottomRightRadius: 5,
-                borderBottomLeftRadius: 5,
-            }}>
+            <View style={styles.textContainer}>
                 <Text
-                    style={{
-                        color: 'white',
-                        fontSize: 13,
-                        textAlign: 'center'
-                    }}
+                    style={styles.texto}
                     numberOfLines={2}
                 >{nombre}</Text>
             </View>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative'
+    },
+    imagen: {
+        width: 100,
+        height: 150,
+        backgroundColor: '#201F29',
+        borderRadius: 5,
+        borderColor: '#fff',
+        borderWidth: 0.5
+    },
+    textContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.15)',
+        padding: 5,
+        borderBottomRightRadius: 5,
+        borderBottomLeftRadius: 5,
+    },
+    texto: {
+        color: 'white',
+        fontSize: 13,
+        textAlign: 'center'
+    },
+});
 
 export default CardActor;
