0f27f2573417b44f086b7b32de8964dd3a063e52
Abnerpino <abnerpino15@gmail.com>
2026-03-19 22:40:12 -0600

    feat(MainActivity): agregar configuración para usar nueva librería KeyEvent para el control de TV

--- android/app/src/main/java/com/iptv_player/MainActivity.kt
+++ android/app/src/main/java/com/iptv_player/MainActivity.kt
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.view.KeyEvent
+import com.github.kevinejohn.keyevent.KeyEventModule
 
 class MainActivity : ReactActivity() {
 
@@ -18,5 +20,15 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  
+  override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    KeyEventModule.getInstance().onKeyDownEvent(keyCode, event)
+    return super.onKeyDown(keyCode, event)
+  }
+
+  override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+    KeyEventModule.getInstance().onKeyUpEvent(keyCode, event)
+    return super.onKeyUp(keyCode, event)
+  }
 }
